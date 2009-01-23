6bb5f6981fb578d0c351eee683b30665bd158875
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-01-23 12:37:37 +0100

    doc: a bit more documentation

--- dfki/base_types.h
+++ dfki/base_types.h
@@ -8,7 +8,9 @@
 
 namespace DFKI {
     struct Time {
+        /** The number of seconds */
         int seconds;
+        /** The number of microseconds. This is always in [0, 1000000]. */
         int microseconds;
 
 #ifndef __orogen
@@ -18,6 +20,7 @@ namespace DFKI {
         explicit Time(int seconds, int microseconds = 0)
             : seconds(seconds), microseconds(microseconds) {}
 
+        /** Returns the current time */
         static Time now() {
             timeval t;
             gettimeofday(&t, 0);
@@ -44,6 +47,7 @@ namespace DFKI {
             result.canonize();
             return result;
         }
+
         Time operator + (Time const& ts) const
         {
             Time result;
@@ -53,19 +57,26 @@ namespace DFKI {
             return result;
         }
 
+        /** True if this time is zero */
         bool isNull() const { return seconds == 0 && microseconds == 0; }
+
+        /** Converts this time as a timeval object */
         timeval toTimeval() const
         {
             timeval tv = { seconds, microseconds };
             return tv;
         }
 
+        /** Returns this time as a fractional number of seconds */
         double toSeconds() const
         {
             return static_cast<double>(seconds) + static_cast<double>(microseconds) / 1000000.0;
         }
 
     private:
+        /** This method makes sure that the constraint on microseconds is met
+         * (i.e. that microseconds is in [0, 1000000].
+         */
         void canonize()
         {
             int const UsecPerSec = 1000000;
