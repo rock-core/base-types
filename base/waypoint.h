460e37996674f6c1748e7d72a67448aab3efd460
Janosch Machowinski <janosch.machowinski@dfki.de>
2010-01-11 18:14:00 +0100

    added new class waypoint
    a class that represents a waypoint

---
+++ base/waypoint.h
@@ -0,0 +1,39 @@
+#ifndef __BASE_WAYPOINT_HH__
+#define __BASE_WAYPOINT_HH__
+
+#ifdef __orogen
+#error "this header cannot be used in orogen-parsed code. Use wrappers/waypoint.h and wrappers::Waypoint instead"
+#endif
+
+#include <Eigen/Core>
+#include <Eigen/Geometry> 
+
+namespace base
+{
+    /**
+     * Representation for a pose
+     */
+    struct Waypoint
+    {
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        Eigen::Vector3d position;
+        //heading in radians
+        double heading;
+
+        //tollerance of the position in m
+        double tol_position;
+        //tollerance of the heading in rad
+        double tol_heading;
+      
+
+        Waypoint()
+	  : position(Position::Identity()), heading(0), tol_position(0), tol_heading(0)  {}
+      
+	Waypoint(Eigen::Vector3d &position, double heading, double tol_position, double tol_heading):
+	    position(position), heading(heading), tol_position(tol_position), tol_heading(tol_heading) {};
+
+    };
+}
+
+#endif
+
