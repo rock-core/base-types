1986cc5598edb2aa4ce788ecb94e20a3d7270fc3
Ajish Babu <ajish.babu@dfki.de>
2010-02-17 16:59:35 +0100

    added enum of wheel IDs

--- base/Robot.hpp
+++ base/Robot.hpp
@@ -11,6 +11,14 @@ namespace base
 	    static const double WIDTH           = 0.300; // [m]
 	    static const double ROTATION_RADIUS = 0.300; // [m]
 	    static const double WHEEL_RADIUS    = 0.150; // [m]
+
+	    enum WHEEL
+	    {
+		REAR_LEFT   = 0,
+		REAR_RIGHT  = 1,
+		FRONT_RIGHT = 2,
+		FRONT_LEFT  = 3
+	    };
 	};
     }
 }
