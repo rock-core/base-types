b6102e329c15f3f2e5537efffdb8b590b27e6293
Ajish Babu <ajish.babu@dfki.de>
2010-02-17 16:09:50 +0100

    moved Robot.hpp from modules/control to base

---
+++ base/Robot.hpp
@@ -0,0 +1,19 @@
+#ifndef ROBOT_HPP
+#define ROBOT_HPP
+
+namespace base
+{
+    namespace robot
+    {
+	class FourWheelGeom
+	{
+	public:
+	    static const double WIDTH           = 0.300; // [m]
+	    static const double ROTATION_RADIUS = 0.300; // [m]
+	    static const double WHEEL_RADIUS    = 0.150; // [m]
+	};
+    }
+}
+
+#endif
+
