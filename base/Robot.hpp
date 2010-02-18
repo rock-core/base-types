bf0470ccf3dbf5a0d7ad621708067970dc7796a5
Ajish Babu <ajish.babu@dfki.de>
2010-02-18 14:08:41 +0100

    added new constants and removed ROTATION_RADIUS

--- base/Robot.hpp
+++ base/Robot.hpp
@@ -8,9 +8,13 @@ namespace base
 	class FourWheelGeom
 	{
 	public:
-	    static const double WIDTH           = 0.300; // [m]
-	    static const double ROTATION_RADIUS = 0.300; // [m]
-	    static const double WHEEL_RADIUS    = 0.150; // [m]
+	    static const double WIDTH		    = 0.300; // To be removed [m]
+	    static const double ROTATION_RADIUS	    = 0.300; // To be removed [m]
+
+	    static const double WHEEL_RADIUS_MAX    = 0.190; // radius at the location of the spike[m]
+	    static const double WHEEL_RADIUS_EFF    = 0.170; //(to be calculated) effective wheel radius for one rotation[m]
+	    static const double TRACK		    = 0.515; // distance between the centers of the left and right wheels.[m]  
+	    static const double WHEEL_BASE	    = 0.510; // Distance between the centers of the front and rear wheels. [m]
 
 	    enum WHEEL
 	    {
