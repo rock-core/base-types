bae9364b8263a2f2a1db32fd77ec0f4af2d17b0f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-03-04 14:23:03 +0100

    replace NURBSCurve3D by a more generic Spline<DIM> template

--- src/spline.cpp
+++ src/spline.cpp
@@ -1,4 +1,4 @@
-#include "NURBSCurve3D.h"
+#include "spline.h"
 #include "sisl/sisl.h"
 
 #include <stdexcept>
@@ -8,19 +8,17 @@ using namespace std;
 using namespace base::geometry;
 using namespace Eigen;
 
-NURBSCurve3D::NURBSCurve3D ( double _geometric_resolution, int _curve_order,
-        std::vector<Eigen::Vector3d> const& points)
-    : curve(0), geometric_resolution(_geometric_resolution)
-    , curve_order(_curve_order), points(points)
+SplineBase::SplineBase (int dim, double _geometric_resolution, int _curve_order)
+    : dimension(dim), curve(0), geometric_resolution(_geometric_resolution)
+    , curve_order(_curve_order)
     , has_curve_length(false), curve_length(-1)
     , has_curvature_max(false), curvature_max(-1)
 {
 }
 
-NURBSCurve3D::NURBSCurve3D(double geometric_resolution, int order,
-        std::vector<Eigen::Vector3d> const& points, SISLCurve* curve)
-    : curve(curve), geometric_resolution(geometric_resolution)
-    , points(points), curve_order(order)
+SplineBase::SplineBase(double geometric_resolution, SISLCurve* curve)
+    : dimension(curve->idim), curve(curve), geometric_resolution(geometric_resolution)
+    , curve_order(curve->ik)
     , has_curve_length(false), curve_length(-1)
     , has_curvature_max(false), curvature_max(-1)
 {
@@ -31,16 +29,16 @@ NURBSCurve3D::NURBSCurve3D(double geometric_resolution, int order,
         throw std::runtime_error("cannot get the curve start & end parameters");
 }
 
-NURBSCurve3D::~NURBSCurve3D ()
+SplineBase::~SplineBase ()
 {
     // Frees the memory of the curve 
     if (curve)
         freeCurve(curve);
 }
 
-NURBSCurve3D::NURBSCurve3D(NURBSCurve3D const& source)
-    : curve(source.curve ? copyCurve(source.curve) : 0)
-    , points(source.points)
+SplineBase::SplineBase(SplineBase const& source)
+    : dimension(source.dimension)
+    , curve(source.curve ? copyCurve(source.curve) : 0)
     , geometric_resolution(source.geometric_resolution)
     , curve_order(source.curve_order)
     , start_param(source.start_param)
@@ -50,22 +48,40 @@ NURBSCurve3D::NURBSCurve3D(NURBSCurve3D const& source)
 {
 }
 
-Vector3d NURBSCurve3D::getPoint(double _param)
+SplineBase const& SplineBase::operator = (SplineBase const& source)
+{
+    if (&source == this)
+        return *this;
+
+    dimension            = source.dimension;
+    curve                = source.curve ? copyCurve(source.curve) : 0;
+    geometric_resolution = source.geometric_resolution;
+    curve_order          = source.curve_order;
+    start_param          = source.start_param;
+    end_param            = source.end_param;
+    has_curve_length     = source.has_curve_length;
+    curve_length         = source.curve_length;
+    has_curvature_max    = source.has_curvature_max;
+    curvature_max        = source.curvature_max;
+    return *this;
+}
+
+int SplineBase::getPointCount() const
+{ return curve->in; }
+
+void SplineBase::getPoint(double* result, double _param)
 {
     if (_param < start_param || _param > end_param) 
         throw std::out_of_range("_param is not in the [start_param, end_param] range");
 
     int leftknot; // Not needed
-    double pos[DIM]; // Array to store the position
     int status;
-    s1227(curve, 0, _param, &leftknot, pos, &status); // Gets the point
+    s1227(curve, 0, _param, &leftknot, result, &status); // Gets the point
     if (status != 0)
         throw std::runtime_error("SISL error while computing a curve point");
-
-    return Vector3d(pos[0], pos[1], pos[2]);
 }
 
-double NURBSCurve3D::getCurvature(double _param)
+double SplineBase::getCurvature(double _param)
 {
     // Limits the input paramter to the curve limit
     if (_param < start_param || _param > end_param) 
@@ -80,7 +96,7 @@ double NURBSCurve3D::getCurvature(double _param)
     return curvature;
 }
 
-double NURBSCurve3D::getVariationOfCurvature(double _param)  // Variation of Curvature
+double SplineBase::getVariationOfCurvature(double _param)  // Variation of Curvature
 {
     if (_param < start_param || _param > end_param) 
         throw std::out_of_range("_param is not in the [start_param, end_param] range");
@@ -94,7 +110,7 @@ double NURBSCurve3D::getVariationOfCurvature(double _param)  // Variation of Cur
     return VoC;
 }
 
-double NURBSCurve3D::getCurveLength()
+double SplineBase::getCurveLength()
 {
     if (has_curve_length)
         return curve_length;
@@ -108,12 +124,12 @@ double NURBSCurve3D::getCurveLength()
     return curve_length;
 }
 
-double NURBSCurve3D::getUnitParameter()
+double SplineBase::getUnitParameter()
 {
     return (end_param - start_param) / getCurveLength();
 }
 
-double NURBSCurve3D::getCurvatureMax()
+double SplineBase::getCurvatureMax()
 {
     if (has_curvature_max)
         return curvature_max;
@@ -132,27 +148,10 @@ double NURBSCurve3D::getCurvatureMax()
     return curvature_max;
 }
 
-void NURBSCurve3D::addPoint(Vector3d pt)
+void SplineBase::interpolate(std::vector<double> const& points)
 {
-    points.push_back(pt);
-}
-
-void NURBSCurve3D::update()
-{
-    vector<double> points;
-    points.reserve(getPointCount() * 3);
     vector<int> point_types;
-    point_types.reserve(getPointCount());
-
-    // Puts all the points into an array
-    for (vector<Vector3d>::iterator it = this->points.begin(); it != this->points.end(); ++it) 
-    {
-	points.push_back(it->x());
-	points.push_back(it->y());
-	points.push_back(it->z());
-
-        point_types.push_back(1); // ordinary point
-    }
+    point_types.resize(points.size() / dimension, 1);
 
     /* 
        Input Arguments:
@@ -210,14 +209,14 @@ void NURBSCurve3D::update()
     }
 
     int status;
-    s1356(&points[0], getPointCount(), DIM, &point_types[0], 0, 0, 1, curve_order, start_param, &end_param, &curve, 
+    s1356(const_cast<double*>(&points[0]), point_types.size(), dimension, &point_types[0], 0, 0, 1, curve_order, start_param, &end_param, &curve, 
 	    &point_param, &nb_unique_param, &status);
     if (status != 0)
         throw std::runtime_error("cannot generate the curve");
 
 }
 
-void NURBSCurve3D::printCurveProperties()
+void SplineBase::printCurveProperties()
 {
     std::cout << "CURVE PROPERTIES " << std::endl
 	<< "  Point count  : " << curve->in    << std::endl
@@ -228,10 +227,8 @@ void NURBSCurve3D::printCurveProperties()
 	<< "  Length       : " << getCurveLength() << std::endl;
 }
 
-double NURBSCurve3D::findOneClosestPoint(Vector3d const& _pt)
-{ return findOneClosestPoint(_pt, geometric_resolution); }
 
-double NURBSCurve3D::findOneClosestPoint(Vector3d const& _pt, double _geores)
+double SplineBase::findOneClosestPoint(double const* _pt, double _geores)
 {
     vector<double> points;
     vector< pair<double, double> > curves;
@@ -246,15 +243,8 @@ double NURBSCurve3D::findOneClosestPoint(Vector3d const& _pt, double _geores)
         return points.front();
 }
 
-void NURBSCurve3D::findClosestPoints(Vector3d const& _pt, vector<double>& _points, vector< pair<double, double> >& _curves)
+void SplineBase::findClosestPoints(double const* ref_point, vector<double>& _result_points, vector< pair<double, double> >& _result_curves, double _geores)
 {
-    return findClosestPoints(_pt, _points, _curves, geometric_resolution);
-}
-
-void NURBSCurve3D::findClosestPoints(Vector3d const& _pt, vector<double>& _result_points, vector< pair<double, double> >& _result_curves, double _geores)
-{
-    double point[DIM] = {_pt.x(),_pt.y(),_pt.z() }; // Array of the point
-
     int points_count;
     double* points;
     int curves_count;
@@ -262,7 +252,7 @@ void NURBSCurve3D::findClosestPoints(Vector3d const& _pt, vector<double>& _resul
 
     // Finds the closest point on the curve
     int status;
-    s1953(curve, point, DIM, _geores, _geores, &points_count, &points, &curves_count, &curves, &status);
+    s1953(curve, const_cast<double*>(ref_point), dimension, _geores, _geores, &points_count, &points, &curves_count, &curves, &status);
     if (status != 0)
         throw std::runtime_error("failed to find the closest points");
 
@@ -275,17 +265,13 @@ void NURBSCurve3D::findClosestPoints(Vector3d const& _pt, vector<double>& _resul
     free(points);
 }
 
-double NURBSCurve3D::localClosestPointSearch(Vector3d const& _pt, double _guess, double _start, double _end)
-{ return localClosestPointSearch(_pt, _guess, _start, _end, geometric_resolution); }
-
-double NURBSCurve3D::localClosestPointSearch(Vector3d const& _pt, double _guess, double _start, double _end, double  _geores)
+double SplineBase::localClosestPointSearch(double* ref_point, double _guess, double _start, double _end, double  _geores)
 {
     double param;
-    double point[DIM] = { _pt.x(),_pt.y(),_pt.z() }; // Array of the point
 
     // Finds the closest point on the curve
     int status;
-    s1774(curve, point, DIM, _geores, _start, _end, _guess, &param, &status);
+    s1774(curve, ref_point, dimension, _geores, _start, _end, _guess, &param, &status);
     if (status < 0)
         throw std::runtime_error("failed to find the closest points");
 
@@ -293,50 +279,23 @@ double NURBSCurve3D::localClosestPointSearch(Vector3d const& _pt, double _guess,
     return param;
 }
 
-Matrix3d NURBSCurve3D::getFrenetFrame(double _param)
-{
-    double p;    // does nothing
-    double t[3], n[3], b[3]; // Frame axis
 
-    // Finds the frenet frame
-    int status;
-    s2559(curve, &_param, 1, &p, t, n, b, &status);
 
-    // Writes the frame to a matrix
-    Matrix3d frame;
-    frame << t[0], t[1], t[2], n[0], n[1], n[2], b[0], b[1], b[2];
-
-    return frame;
-}
-
-double NURBSCurve3D::getHeading(double _param)
-{    
-    Matrix3d frame = getFrenetFrame(_param);
-
-    // Vector if the X axis of the frame
-    Vector2d Xaxis(frame(0,0),frame(0,1));
-    Xaxis.normalize(); 
-
-    // Returns the angle of Frenet X axis in Inertial frame
-    return atan2(Xaxis.y(),Xaxis.x());
-}
-
-void NURBSCurve3D::clear()
+void SplineBase::clear()
 {
     if (curve)
     {
         freeCurve(curve);
         curve = 0;
     }
-    points.clear();
 }
 
-vector<double> NURBSCurve3D::simplify()
+vector<double> SplineBase::simplify()
 {
     return simplify(geometric_resolution);
 }
 
-vector<double> NURBSCurve3D::simplify(double tolerance)
+vector<double> SplineBase::simplify(double tolerance)
 {
     if (!curve)
         throw std::runtime_error("the curve is not initialized");
@@ -360,7 +319,45 @@ vector<double> NURBSCurve3D::simplify(double tolerance)
     return vector<double>(maxerr, maxerr + 3);
 }
 
-double NURBSCurve3D::headingError(double _actHeading, double _param)
+SISLCurve const* SplineBase::getSISLCurve() const
+{
+    return curve;
+}
+
+SISLCurve* SplineBase::getSISLCurve()
+{
+    return curve;
+}
+
+Eigen::Matrix3d SplineBase::getFrenetFrame(double _param)
+{
+    double p;    // does nothing
+    double t[3], n[3], b[3]; // Frame axis
+
+    // Finds the frenet frame
+    int status;
+    s2559(curve, &_param, 1, &p, t, n, b, &status);
+
+    // Writes the frame to a matrix
+    Matrix3d frame;
+    frame << t[0], t[1], t[2], n[0], n[1], n[2], b[0], b[1], b[2];
+
+    return frame;
+}
+
+double SplineBase::getHeading(double _param)
+{    
+    Matrix3d frame = getFrenetFrame(_param);
+
+    // Vector if the X axis of the frame
+    Vector2d Xaxis(frame(0,0),frame(0,1));
+    Xaxis.normalize(); 
+
+    // Returns the angle of Frenet X axis in Inertial frame
+    return atan2(Xaxis.y(),Xaxis.x());
+}
+
+double SplineBase::headingError(double _actHeading, double _param)
 {
     // Orientation error
     double error = _actHeading - getHeading(_param);
@@ -372,10 +369,12 @@ double NURBSCurve3D::headingError(double _actHeading, double _param)
      	return error;
 }
 
-double NURBSCurve3D::distanceError(Vector3d _pt, double _param)
+double SplineBase::distanceError(Eigen::Vector3d _pt, double _param)
 {
     // Error vector
-    Vector3d error = _pt-getPoint(_param);
+    Vector3d curve_point;
+    getPoint(curve_point.data(), _param);
+    Vector3d error = _pt - curve_point;
     error(2) = 0.0;  // Z axis error not needed
 
     // Finds the angle of error vector to the Frenet X axis 
@@ -388,25 +387,18 @@ double NURBSCurve3D::distanceError(Vector3d _pt, double _param)
     return (angle >= 0.0)?(error.norm()):(-error.norm());
 }
 
-Vector3d NURBSCurve3D::poseError(Vector3d _pt, double _actZRot, double _st_para, double _len_tol)
+Eigen::Vector3d SplineBase::poseError(Eigen::Vector3d _pt, double _actZRot, double _st_para, double _len_tol)
 {
     // Finds the search length
     double del_para = getUnitParameter() *  _len_tol;
 
     // Finds teh closest poiont in the search length
-    double param = localClosestPointSearch(_pt, _st_para, _st_para, _st_para + del_para);    
+    double param = localClosestPointSearch(_pt.data(), _st_para, _st_para, _st_para + del_para, getGeometricResolution());
 
     // Returns the error [distance error, orientation error, parameter] 
-    return Vector3d(distanceError(_pt, param), headingError(_actZRot, param), param);
+    return Eigen::Vector3d(distanceError(_pt, param), headingError(_actZRot, param), param);
 }
 
-SISLCurve const* NURBSCurve3D::getSISLCurve() const
-{
-    return curve;
-}
 
-SISLCurve* NURBSCurve3D::getSISLCurve()
-{
-    return curve;
-}
+
 
---
+++ src/spline.h
@@ -0,0 +1,253 @@
+#ifndef  _BASE_SPLINE_HPP_INC
+#define  _BASE_SPLINE_HPP_INC
+
+#include <vector>
+#include <eigen2/Eigen/Core>
+
+#include <boost/utility/enable_if.hpp>
+
+struct SISLCurve;
+
+namespace base {
+namespace geometry {
+    /** Representation and manipulation of a N-order, 3D NURBS curve
+     *
+     * The availability of this class is dependent on the availability of the
+     * SISL library
+     *
+     * http://www.sintef.no/Informasjons--og-kommunikasjonsteknologi-IKT/Anvendt-matematikk/Fagomrader/Geometri/Prosjekter/The-SISL-Nurbs-Library/SISL-Homepage/
+     */
+    class SplineBase
+    {
+        SplineBase();
+    public:
+        SplineBase(SplineBase const& source);
+        virtual ~SplineBase();
+
+        explicit SplineBase(int dimension,
+                double geometric_resolution = 0.1, int order = 3);
+        explicit SplineBase(double geometric_resolution, SISLCurve* curve);
+
+        /** Changes the default geometric resolution */
+        void setGeometricResolution(double _geores) { geometric_resolution = _geores; }
+        double getGeometricResolution() const { return geometric_resolution; };
+
+        /** Returns the dimension of the space in which the curve lies */
+        int    getDimension() const { return dimension; }
+        /** Returns the number of points for this curve */
+        int    getPointCount() const;
+        /** Returns the order of the curve */
+        int    getCurveOrder() const { return curve_order; }
+        /** Returns the length of the curve in geometric space */
+        double getCurveLength();
+        /** Returns the maximum curvature of the curve */
+        double getCurvatureMax();
+        double getStartParam() const { return start_param; };
+        double getEndParam()   const { return end_param; };
+
+        /** Return a pointer to the underlying SISL structure
+         *
+         * This pointer will be non-NULL only after interpolate() has been called
+         * at least once.
+         */
+        SISLCurve const* getSISLCurve() const;
+
+        /** Return a pointer to the underlying SISL structure
+         *
+         * This pointer will be non-NULL only after interpolate() has been called
+         * at least once.
+         */
+        SISLCurve* getSISLCurve();
+
+        /** Returns the length-to-parametric scale
+         *
+         * I.e. it returns the number of parametric units that lie in one
+         * curve length unit
+         */
+        double getUnitParameter();
+      
+        /** Returns the curvature at the given position
+         *
+         * @throws out_of_range if _param is not in [start_param,
+         * end_param] and runtime_error if SISL returns an error
+         */
+        double getCurvature(double _param);
+
+        /** Returns the first order derivative of the curvature at the given
+         * position
+         *
+         * @throws out_of_range if _param is not in [start_param,
+         * end_param] and runtime_error if SISL returns an error
+         */
+        double getVariationOfCurvature(double _param);  // Variation of Curvature
+                
+        /** Returns the maximum curvature in a curve
+         *
+         * Iterates through the curve and return the maximum curvature found
+         *  Checks every delLen for the parameter
+         */
+        double findCurvatureMax(); 
+
+        void printCurveProperties();
+
+        /** Generates the curve */
+        void interpolate(std::vector<double> const& coordinates);
+
+        /** Reinitializes the curve */
+        void clear();
+
+        std::vector<double> simplify();
+        std::vector<double> simplify(double tolerance);
+
+        SplineBase const& operator = (SplineBase const& base);
+
+    protected:
+        void getPoint(double* result, double _param);
+        double findOneClosestPoint(double const* _pt, double _geores);
+        void findClosestPoints(double const* ref_point,
+                std::vector<double>& _result_points,
+                std::vector< std::pair<double, double> >& _result_curves,
+                double _geores);
+
+        double localClosestPointSearch(double* ref_point,
+                double _guess, double _start, double _end,
+                double  _geores);
+
+        //! available only in Spline<3>
+        Eigen::Matrix3d getFrenetFrame(double _param);
+        //! available only in Spline<3>
+        double getHeading(double _param);
+        //! available only in Spline<3>
+        double headingError(double _actHeading, double _param);
+        //! available only in Spline<3>
+        double distanceError(Eigen::Vector3d _pt, double _param);
+        //! available only in Spline<3>
+        Eigen::Vector3d poseError(Eigen::Vector3d _pt, double _actZRot, double _st_para, double _len_tol);;
+
+
+    private:
+        int dimension;
+        //! the underlying SISL curve
+        SISLCurve *curve;
+
+        //! the geometric resolution
+        double geometric_resolution;
+        //! the order of the NURBS curve that approximates the points
+        int curve_order;
+        //! the start parameter (usually 0.0)
+        double start_param;
+        //! the end parameter, as returned by SISL
+        double end_param;
+
+        //! if we have already calculated the curve length
+        bool has_curve_length;
+        //! the cache value for the curve length in geometric space
+        double curve_length; // Length of the curve
+
+        //! if we have already calculated the maximum curvature
+        bool has_curvature_max;
+        //! maximum curvature in the curve
+        double curvature_max;
+    };
+
+    template<int DIM>
+    class Spline : public SplineBase
+    {
+    public:
+        typedef Eigen::Matrix<double, DIM, 1> vector_t;
+
+        explicit Spline(double geometric_resolution = 0.1, int order = 3)
+            : SplineBase(DIM, geometric_resolution, order) {}
+        explicit Spline(double geometric_resolution, SISLCurve* curve)
+            : SplineBase(geometric_resolution, curve) {}
+        Spline(SplineBase const& base)
+            : SplineBase(base) {}
+
+        /** Returns the geometric point that lies on the curve at the given
+         * parameter */
+        vector_t getPoint(double _param)
+        { 
+            vector_t result;
+            SplineBase::getPoint(result.data(), _param);
+        }
+
+        /** \overload
+         */
+        double findOneClosestPoint(vector_t const& _pt)
+        { return findOneClosestPoint(_pt, getGeometricResolution()); }
+
+        /** Compute the curve from the given set of points */
+        void interpolate(std::vector<vector_t> const& points)
+        {
+            std::vector<double> coordinates;
+            for (int i = 0; i < points.size(); ++i)
+                coordinates.insert(coordinates.end(), points[i].data(), points[i].data() + getDimension());
+            SplineBase::interpolate(coordinates);
+        }
+
+        /** Returns a single closest point to _pt
+         *
+         * This is a convenience method that calls findClosestPoints
+         *
+         * @return the parameter of the found closes point
+         * @throw std::runtime_error if no points have been found (should not happen)
+         * @see localClosestPointSearch findClosestPoints
+         */
+        double findOneClosestPoint(vector_t const& _pt, double _geometric_resolution)
+        { return SplineBase::findOneClosestPoint(_pt.data(), _geometric_resolution); }
+
+        /** \overload
+         */
+        void findClosestPoints(vector_t const& _pt,
+                std::vector<double>& _points,
+                std::vector< std::pair<double, double> >& _curves)
+        { return findClosestPoints(_pt, _points, _curves, getGeometricResolution()); }
+
+        /**
+         * Returns the single points and curve segments that are closest to
+         * the given point.
+         *
+         */
+        void findClosestPoints(vector_t const& _pt,
+                std::vector<double>& _points,
+                std::vector< std::pair<double, double> >& _curves,
+                double _geores)
+        { return SplineBase::findClosestPoints(_pt.data(), _points, _curves, _geores); }
+
+        /** \overload
+         */
+        double localClosestPointSearch(vector_t const& _pt, double _guess, double _start, double _end)
+        { return localClosestPointSearch(_pt, _guess, _start, _end, getGeometricResolution()); }
+
+        /** Performs a Newton search in the provided parametric interval, starting with the given guess.
+         *
+         * This method is subject to local minima problems
+         */
+        double localClosestPointSearch(vector_t const& _pt, double _guess, double _start, double _end, double _geores)
+        { return SplineBase::localClosestPointSearch(_pt.data(), _guess, _start, _end, _geores); }
+
+        Eigen::Matrix3d getFrenetFrame(double _param,
+                typename boost::enable_if_c<DIM == 3>::type* enabler = 0)
+        { return SplineBase::getFrenetFrame(_param); }
+
+        double getHeading(double _param,
+                typename boost::enable_if_c<DIM == 3>::type* enabler = 0)
+        { return SplineBase::getHeading(_param); }
+
+        double headingError(double _actHeading, double _param,
+                typename boost::enable_if_c<DIM == 3>::type* enabler = 0)
+        { return SplineBase::headingError(_actHeading, _param); }
+
+        double distanceError(Eigen::Vector3d _pt, double _param,
+                typename boost::enable_if_c<DIM == 3>::type* enabler = 0)
+        { return SplineBase::distanceError(_pt, _param); }
+
+        Eigen::Vector3d poseError(Eigen::Vector3d _pt, double _actZRot, double _st_para, double _len_tol,
+                typename boost::enable_if_c<DIM == 3>::type* enabler = 0)
+        { return SplineBase::poseError(_pt, _actZRot, _st_para, _len_tol); }
+    };
+
+    typedef Spline<3> NURBSCurve3D;
+} // geometry
+} // base
+#endif
