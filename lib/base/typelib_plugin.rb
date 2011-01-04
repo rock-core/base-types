a7ce90f6614080627ef6b7f86ada8f8c839db522
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-01-04 16:08:01 +0100

    use the new :if selectors to select between "old" base/Time and "new" base/Time

--- lib/base/typelib_plugin.rb
+++ lib/base/typelib_plugin.rb
@@ -1,22 +1,22 @@
-##
-# base/Time to Ruby Time convertions
-Typelib.convert_to_ruby '/base/Time' do |value|
-    if value.respond_to?(:seconds)
-        Time.at(value.seconds, value.microseconds)
-    else
-        microseconds = value.microseconds
-        seconds = microseconds / 1_000_000
-        Time.at(seconds, microseconds % 1_000_000)
-    end
+# If we get a /base/Time, convert it to Ruby's Time class
+Typelib.convert_to_ruby '/base/Time', :if => lambda { |t| t.has_field?('seconds') } do |value|
+    Time.at(value.seconds, value.microseconds)
 end
-Typelib.convert_from_ruby Time, '/base/Time' do |value, typelib_type|
+Typelib.convert_to_ruby '/base/Time', :if => lambda { |t| !t.has_field?('seconds') } do |value|
+    microseconds = value.microseconds
+    seconds = microseconds / 1_000_000
+    Time.at(seconds, microseconds % 1_000_000)
+end
+# Tell Typelib that Time instances can be converted into /base/Time values
+Typelib.convert_from_ruby Time, '/base/Time', :if => lambda { |t| t.has_field?('seconds') } do |value, typelib_type|
     result = typelib_type.new
-    if result.respond_to?(:seconds)
-        result.seconds      = value.tv_sec
-        result.microseconds = value.tv_usec
-    else
-        result.microseconds = value.tv_sec * 1_000_000 + value.tv_usec
-    end
+    result.seconds      = value.tv_sec
+    result.microseconds = value.tv_usec
+    result
+end
+Typelib.convert_from_ruby Time, '/base/Time', :if => lambda { |t| !t.has_field?('seconds') } do |value, typelib_type|
+    result = typelib_type.new
+    result.microseconds = value.tv_sec * 1_000_000 + value.tv_usec
     result
 end
 
