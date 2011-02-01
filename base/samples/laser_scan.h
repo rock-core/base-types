98ea4ea0d286fcb057d409dc41ca5512bc04ad05
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2011-02-01 16:02:01 +0100

    added default Transformation for Pointcloude

--- base/samples/laser_scan.h
+++ base/samples/laser_scan.h
@@ -94,7 +94,7 @@ namespace base { namespace samples {
 	    return true;
 	}
 	
-	std::vector<Eigen::Vector3d> convertScanToPointCloud(const Eigen::Transform3d& transform) const
+	std::vector<Eigen::Vector3d> convertScanToPointCloud(const Eigen::Transform3d& transform=Eigen::Transform3d()) const
 	{
 	    std::vector<Eigen::Vector3d> pointCloud;
 	    
