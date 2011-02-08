ddcd01892475bbdf460d85100f243aa9df360bf4
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-02-08 15:49:02 +0100

    [angle] made rad public to allow usage as interface type
    * added some documentation

--- base/angle.h
+++ base/angle.h
@@ -6,12 +6,24 @@
 namespace base
 {
 
+/** 
+ * This class represents an angle, and can be used instead of double for
+ * convenience. The class has a canonical representation of the angle in
+ * degrees, in the interval PI < rad <= PI. 
+ */
 class Angle
 {
-    // angle in radians 
-    // will always be PI < rad <= PI
+public:
+    /** 
+     * angle in radians.
+     * this value will always be PI < rad <= PI
+     *
+     * @note don't use this value directly. It's only public to allow this class
+     * to be used as an interface type.
+     */
     double rad;
     
+protected:
     explicit Angle( double rad ) : rad(rad) 
     { 
 	canonize(); 
@@ -28,26 +40,48 @@ class Angle
     }
 
 public:
+    /** 
+     * use this method to get an angle from radians.
+     * @return representation of the given angle.
+     * @param rad - angle in radians.
+     */
     static inline Angle fromRad( double rad )
     {
 	return Angle( rad );
     }
 
+    /** 
+     * use this method to get an angle from degrees.
+     * @return representation of the given angle.
+     * @param deg - angle in degrees.
+     */
     static inline Angle fromDeg( double deg )
     {
 	return Angle( deg / 180.0 * M_PI );
     }
 
+    /**
+     * @return canonical value of the angle in radians
+     */
     double inline getRad() const 
     {
 	return rad;
     }
 
+    /**
+     * @return canonical value of the angle in degrees
+     */
     double inline getDeg() const
     {
 	return rad / M_PI * 180;
     }
 
+    /**
+     * compare two angles for approximate equality
+     * @param other - angle to compare
+     * @param prec - precision interval in deg
+     * @return true if angle is approximately equal 
+     */
     bool inline isApprox( Angle other, double prec = 1e-5 )
     {
 	return fabs( other.rad - rad ) < prec;
