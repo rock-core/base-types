81b712fb3369c57bd8cc39da305aee3b630d10dc
Alexander Duda <Alexander.Duda@dfki.de>
2012-02-21 19:31:32 +0100

    SonarBeam: add methods for copy and swap

--- base/samples/sonar_beam.h
+++ base/samples/sonar_beam.h
@@ -40,6 +40,11 @@ namespace base { namespace samples {
             speed_of_sound(std::numeric_limits<float>::signaling_NaN()), 
             beamwidth_horizontal(std::numeric_limits<float>::signaling_NaN()), 
             beamwidth_vertical(std::numeric_limits<float>::signaling_NaN()){} 
+
+        SonarBeam(const SonarBeam &other)
+        {
+            init(other);
+        }
             
         //calculates the spatial resolution of the sonar beam in meter
         //this takes the sampling_interval and the speed of sound into account
@@ -49,6 +54,48 @@ namespace base { namespace samples {
             //the sound traveling from the transiter to the target an back
             //to receiver
             return sampling_interval*0.5*speed_of_sound;
+        }
+
+        SonarBeam &operator=(const SonarBeam &other)
+        {
+            init(other);
+            return *this;
+        }
+
+        void init(const SonarBeam &other)
+        {
+            time = other.time;
+            bearing = other.bearing;
+            sampling_interval = other.sampling_interval;
+            speed_of_sound = other.speed_of_sound;
+            beamwidth_horizontal = other.beamwidth_horizontal;
+            beamwidth_vertical = other.beamwidth_vertical;
+            beam = other.beam;
+        }
+
+        void swap(SonarBeam &other)
+        {
+            Time temp_time = time;
+            Angle temp_bearing = bearing;
+            double temp_sampling_interval = sampling_interval;
+            float temp_speed_of_sound = speed_of_sound;
+            float temp_beamwidth_horizontal = beamwidth_horizontal;
+            float temp_beamwidth_vertical= beamwidth_vertical;
+
+            time = other.time;
+            bearing = other.bearing;
+            sampling_interval = other.sampling_interval;
+            speed_of_sound = other.speed_of_sound;
+            beamwidth_horizontal = other.beamwidth_horizontal;
+            beamwidth_vertical = other.beamwidth_vertical;
+            beam.swap(other.beam);
+
+            other.time = temp_time;
+            other.bearing = temp_bearing;
+            other.sampling_interval = temp_sampling_interval;
+            other.speed_of_sound = temp_speed_of_sound;
+            other.beamwidth_horizontal = temp_beamwidth_horizontal;
+            other.beamwidth_vertical = temp_beamwidth_vertical;
         }
     };
 }} 
