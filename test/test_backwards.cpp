17ec9a54008989e06fc0b0e3d141657684954fc9
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-05-29 15:12:46 +0200

    move the backward-compatibility headers in base/backward/base
    This is so that -I${includedir}/base/backward allows to include
    "base/old_header_name.h"

--- test/test_backwards.cpp
+++ test/test_backwards.cpp
@@ -1,36 +1,36 @@
-#include <base/backward/actuators/commands.h>
-#include <base/backward/actuators/status.h>
-#include <base/backward/actuators/vehicles.h>
-#include <base/backward/angle.h>
-#include <base/backward/circular_buffer.h>
-//#includebase/ <backwads/compressed_frame.h>
-#include <base/backward/eigen.h>
-#include <base/backward/float.h>
-#include <base/backward/joint_state.h>
-#include <base/backward/motion_command.h>
-#include <base/backward/odometry.h>
-#include <base/backward/pose.h>
-#include <base/backward/samples/distance_image.h>
-#include <base/backward/samples/frame.h>
-#include <base/backward/samples/imu.h>
-#include <base/backward/samples/joints.h>
-#include <base/backward/samples/laser_scan.h>
-#include <base/backward/samples/pointcloud.h>
-#include <base/backward/samples/rigid_body_acceleration.h>
-#include <base/backward/samples/rigid_body_state.h>
-#include <base/backward/samples/sonar_beam.h>
-#include <base/backward/samples/sonar_scan.h>
-#include <base/backward/temperature.h>
-#include <base/backward/time.h>
-#include <base/backward/timemark.h>
-#include <base/backward/trajectory.h>
-#include <base/backward/waypoint.h>
+#include <base/backward/base/actuators/commands.h>
+#include <base/backward/base/actuators/status.h>
+#include <base/backward/base/actuators/vehicles.h>
+#include <base/backward/base/angle.h>
+#include <base/backward/base/circular_buffer.h>
+//#includebase/ <backwadbase/s/compressed_frame.h>
+#include <base/backward/base/eigen.h>
+#include <base/backward/base/float.h>
+#include <base/backward/base/joint_state.h>
+#include <base/backward/base/motion_command.h>
+#include <base/backward/base/odometry.h>
+#include <base/backward/base/pose.h>
+#include <base/backward/base/samples/distance_image.h>
+#include <base/backward/base/samples/frame.h>
+#include <base/backward/base/samples/imu.h>
+#include <base/backward/base/samples/joints.h>
+#include <base/backward/base/samples/laser_scan.h>
+#include <base/backward/base/samples/pointcloud.h>
+#include <base/backward/base/samples/rigid_body_acceleration.h>
+#include <base/backward/base/samples/rigid_body_state.h>
+#include <base/backward/base/samples/sonar_beam.h>
+#include <base/backward/base/samples/sonar_scan.h>
+#include <base/backward/base/temperature.h>
+#include <base/backward/base/time.h>
+#include <base/backward/base/timemark.h>
+#include <base/backward/base/trajectory.h>
+#include <base/backward/base/waypoint.h>
 
 #ifdef SISL_FOUND
-#include <base/backward/trajectory.h>
+#include <base/backward/base/trajectory.h>
 #endif
 
 #define BASE_LOG_DEBUG
-#include <base/backward/logging.h>
+#include <base/backward/base/logging.h>
 
 
