7193e43ebb6e577dcff2a7b63aff2dc357210d4f
Jakob Schwendner <jakob.schwendner@dfki.de>
2013-07-09 09:37:54 +0200

    moved most of base/samples/Joints into new NamedVector base class
    effectively having a named vector index is a structure that was needed
    for other types as well, so made it a templated base class.

---
+++ base/NamedVector.hpp
@@ -0,0 +1,102 @@
+#ifndef BASE_NAMED_VECTOR_HPP
+#define BASE_NAMED_VECTOR_HPP
+
+#include <stdexcept>
+#include <vector>
+
+namespace base
+{
+    template <class T> 
+    struct NamedVector
+    {
+	/** Exception thrown when trying to find the index of an element by
+	 * name, but the name does not exist
+	 */
+	struct InvalidName : public std::runtime_error
+	{
+	    std::string name;
+	    InvalidName(std::string const& name)
+		: std::runtime_error("trying to access element " + name + 
+			", but there is no element with that name on this structure")
+		  , name(name) {}
+
+	    ~InvalidName() throw() {}
+	};
+
+	/** The names of the elements described in this structure, in the same
+	 * order than the 'elements' field below
+	 */
+	std::vector<std::string> names;
+
+	/** The element vector */
+	std::vector<T> elements;
+
+	/** Returns the state information for the given element */
+	const T& getElementByName(std::string name) const
+	{
+	    return elements.at( mapNameToIndex( name ) );
+	}
+
+	/** convenience method, does the same as the getElementByName */
+	const T& operator[](std::string name) const
+	{
+	    return elements.at( mapNameToIndex( name ) );
+	}
+
+	/** convenience method, does the same as the getElementByName */
+	T& operator[](std::string name)
+	{
+	    return elements.at( mapNameToIndex( name ) );
+	}
+
+	/** returns the element with the given index */
+	const T& operator[]( size_t index ) const
+	{
+	    return elements.at( index );
+	}
+
+	/** returns the element with the given index */
+	T& operator[]( size_t index )
+	{
+	    return elements.at( index );
+	}
+
+	/** Resize the elements vector to this size
+	*/
+	void resize(size_t size)
+	{
+	    elements.resize(size);
+	    names.resize(size);
+	}
+
+	/** Returns the number of elements reported in this structure
+	*/
+	size_t size() const
+	{
+	    return elements.size();
+	}
+
+	/** Clears the contents of elements and names vector
+	*/
+	void clear()
+	{
+	    elements.clear();
+	    names.clear();
+	}
+
+	/** Returns the index that corresponds to the given name
+	 *
+	 * @throws InvalidName if the given name does not exist on this array
+	 */
+	size_t mapNameToIndex(std::string const& name) const
+	{
+	    std::vector<std::string>::const_iterator it = find(names.begin(), names.end(), name);
+	    if (it == names.end())
+		throw InvalidName(name);
+	    return it - names.begin();
+	}
+    };
+}
+
+#endif
+
--- base/samples/Joints.hpp
+++ base/samples/Joints.hpp
@@ -6,6 +6,7 @@
 
 #include <base/Time.hpp>
 #include <base/JointState.hpp>
+#include <base/NamedVector.hpp>
 
 namespace base
 {
@@ -13,73 +14,10 @@ namespace base
     {
         /** Data structure that gives out state readings for a set of joints
          */
-        struct Joints
+        struct Joints : public base::NamedVector<JointState>
         {
-            /** Exception thrown when trying to find the index of a joint by
-             * name, but the name does not exist
-             */
-            struct InvalidName : public std::runtime_error
-            {
-                std::string name;
-                InvalidName(std::string const& name)
-                    : std::runtime_error("trying to access joint " + name + ", but there is no joint with that name on this Joints structure")
-                    , name(name) {}
-
-                ~InvalidName() throw() {}
-            };
-
             /** The sample timestamp */
             base::Time time;
-
-            /** The names of the joints described in this structure, in the same
-             * order than the 'states' field below
-             */
-            std::vector<std::string> names;
-
-            /** The joint state information */
-            std::vector<JointState> states;
-
-            /** Returns the joint state information for the given joint */
-            JointState getStateByName(std::string jointName) const
-            {
-		return states.at( mapNameToIndex( jointName ) );
-            }
-
-            /** Resize the states vector to this size
-             */
-            void resize(size_t size)
-            {
-                states.resize(size);
-                names.resize(size);
-            }
-
-            /** Returns the number of joints reported in this structure
-             */
-            size_t size() const
-            {
-                return states.size();
-            }
-
-	    /** Clears the contents of states and names vector
-	     */
-	    void clear()
-	    {
-		states.clear();
-		names.clear();
-	    }
-
-            /** Returns the joint index that corresponds to the given name
-             *
-             * @throws InvalidName if the given name does not exist on this
-             *   Joints
-             */
-            size_t mapNameToIndex(std::string const& name) const
-            {
-                std::vector<std::string>::const_iterator it = find(names.begin(), names.end(), name);
-                if (it == names.end())
-                    throw InvalidName(name);
-                return it - names.begin();
-            }
         };
     }
 }
