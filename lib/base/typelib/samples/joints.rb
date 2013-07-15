b1cd4d45ee399f4ec65e7b5d449ae9e954f8581f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-07-15 17:30:31 +0200

    update to the new NamedVector<T> base class for base::samples::Joints

--- lib/base/typelib/samples/joints.rb
+++ lib/base/typelib/samples/joints.rb
@@ -3,11 +3,14 @@ require 'base/float'
 Typelib.specialize_model '/base/samples/Joints' do
     def from_positions(*positions)
         states = positions.map do |p|
-            j = self[:states].deference.new
+            j = self[:elements].deference.new
             j.position = p
             j
         end
-        new(:states => states)
+        new(:elements => states)
     end
 end
 
+Typelib.specialize '/base/samples/Joints' do
+    include Base::NamedVector
+end
--- lib/base/typelib_plugin.rb
+++ lib/base/typelib_plugin.rb
@@ -3,6 +3,7 @@ require 'base/typelib/joint_state'
 require 'base/typelib/time'
 require 'base/typelib/rigid_body_state'
 require 'base/typelib/spline'
+require 'base/typelib/named_vector'
 require 'base/typelib/samples/joints'
 
 ##
