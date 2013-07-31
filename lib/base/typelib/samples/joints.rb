e1051b7b86bdf1820bfed2bf47e4fb0bac486b6e
Leif Christensen <leif.christensen@dfki.de>
2013-07-31 14:11:03 +0200

    added some helper functions for base/samples/Joints

--- lib/base/typelib/samples/joints.rb
+++ lib/base/typelib/samples/joints.rb
@@ -9,6 +9,33 @@ Typelib.specialize_model '/base/samples/Joints' do
         end
         new(:elements => states)
     end
+
+    def from_speeds(*speeds)
+        states = speeds.map do |s|
+            j = self[:elements].deference.new
+            j.speed = s
+            j
+        end
+        new(:elements => states)
+    end
+
+    def from_efforts(*efforts)
+        states = efforts.map do |e|
+            j = self[:elements].deference.new
+            j.effort = e
+            j
+        end
+        new(:elements => states)
+    end
+
+    def from_raws(*raws)
+        states = raws.map do |r|
+            j = self[:elements].deference.new
+            j.raw = r
+            j
+        end
+        new(:elements => states)
+    end
 end
 
 Typelib.specialize '/base/samples/Joints' do
