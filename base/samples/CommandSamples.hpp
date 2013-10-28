4a56cd230beef38b261a124fe4bf27c374c73d6b
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2013-10-28 17:52:27 +0100

    modified TimeStamped template and CommandSamples

--- base/samples/CommandSamples.hpp
+++ base/samples/CommandSamples.hpp
@@ -7,6 +7,9 @@
  *  A collection of base/commands with timestamps, for use as command feedback and logging
  *  (which command was executed when) or for supervising tasks.
  *
+ * all definitions may be empty : "class Motion2D : public TimeStamped< commands::Motion2D >{};",
+ * when only set() and getBase() of the TimeStamped template are used in tasks
+ *
  */
 
 #ifndef COMMANDSAMPLES_HPP_
@@ -21,10 +24,23 @@ namespace base{
 	/**
 	 * Time stamped version of a Motion2D command
 	 */
-	class Motion2D : public TimeStamped< commands::Motion2D >{};
+	class Motion2D : public TimeStamped< commands::Motion2D >{
+	public:
+		/**
+		 * default constructor (not needed when no other constructor is present)
+		 */
+		Motion2D ():TimeStamped< commands::Motion2D >(){}
+		/**
+		 * constructor using the base class (allows to write the command directly to the sample port)
+		 */
+		Motion2D (const commands::Motion2D &from):TimeStamped< commands::Motion2D >(){
+			this->set(from);
+		}
+	};
 
-
-	}//namespace templates
+	}//namespace samples
 }//namespace base
 
-#endif /* TIMESTAMPEDDATA_HPP_ */
+
+
+#endif /* COMMANDSAMPLES_HPP_ */
