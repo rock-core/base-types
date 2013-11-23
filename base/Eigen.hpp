27a559090863bd5ba4891d61334e38e481be012d
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2013-11-23 16:38:41 +0100

    Eigen base types: added isnotnan and isfinite methods

--- base/Eigen.hpp
+++ base/Eigen.hpp
@@ -35,6 +35,21 @@ namespace base
 							      Affine3d;
     // alias for backward compatibility
     typedef Affine3d					   Transform3d;
+
+    /**
+     * @brief Check if NaN values
+     */
+    template<typename _Derived>
+    static inline bool isnotnan(const Eigen::MatrixBase<_Derived>& x)
+    {
+        return ((x.array() == x.array())).all();
+    };
+
+    template<typename _Derived>
+    static inline bool isfinite(const Eigen::MatrixBase<_Derived>& x)
+    {
+        return isnotnan(x - x);
+    };
 }
 
 #endif
