293c1ab9f65fcb1600a1034fd649e8241719617f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-01-08 17:42:28 +0100

    test: use the shared library for boost::test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_MODULE BaseTypes
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include <base/Angle.hpp>
 #include <base/commands/AUVMotion.hpp>
