fb2666e7f01b7e7c35338d5b1d32dc917ee8a2d4
Stefan Haase <stefan.haase@dfki.de>
2014-02-24 20:53:02 +0100

    Replaces the current waypoint visualization.
    Shows the heading of each waypoint and vectors of waypoints can be visualized now.

--- viz/WaypointVisualization.cpp
+++ viz/WaypointVisualization.cpp
@@ -1,62 +1,112 @@
 #include "WaypointVisualization.hpp"
-#include <iostream>
 
-namespace vizkit3d 
-{
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/Group>
+#include <osg/ShapeDrawable>
+
+#include <vizkit3d/Vizkit3DHelper.hpp>
+
+using namespace vizkit3d;
+
+struct WaypointVisualization::Data {
+    // Copy of the value given to updateDataIntern.
+    // Making a copy is required because of how OSG works
+    std::vector<base::Waypoint> data;
+};
+
 
 WaypointVisualization::WaypointVisualization()
-{    
-    color = osg::Vec4( 1, 0, 0, 1 );
-    
-    waypoint.tol_position = 0.0;
+    : p(new Data), color(1.0f, 0.0f, 0.0f, 1.0f)
+{
 }
 
-void WaypointVisualization::updateWp(const base::Waypoint& wp)
+WaypointVisualization::~WaypointVisualization()
 {
-    updateData(wp);
+    delete p;
 }
 
-osg::ref_ptr< osg::Node > WaypointVisualization::createMainNode()
+void WaypointVisualization::setColor(QColor q_color)
 {
-    geode = new osg::Geode;
-
-//     osg::StateSet* stategeode = geode->getOrCreateStateSet();
-//     stategeode->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
-
-    waypointPosition = new osg::PositionAttitudeTransform();
-    
-    sphere = new osg::Sphere(osg::Vec3f(0,0,0),0.3);
-    sphere->setDataVariance(osg::Object::DYNAMIC);
-    sphereDrawable = new osg::ShapeDrawable(sphere);
-    sphereDrawable->setColor(color);
-    sphereDrawable->setDataVariance(osg::Object::DYNAMIC);
-    
-    return geode;
-}
-
-void WaypointVisualization::setColor(const base::Vector3d& color)
-{
-    this->color = osg::Vec4d(color.x(), color.y(), color.z(), 1);
+    color = osg::Vec4(q_color.redF(), q_color.greenF(), 
+            q_color.blueF(), q_color.alphaF());
     setDirty();
 }
 
-void WaypointVisualization::updateDataIntern ( const base::Waypoint& data )
+QColor WaypointVisualization::getColor() const
 {
-    waypoint = data;
+    QColor q_color;
+    q_color.setRgbF(color[0], color[1], color[2], color[3]);
+    return q_color;
+} 
+
+osg::ref_ptr<osg::Node> WaypointVisualization::createMainNode()
+{
+    osg::ref_ptr<osg::Group> waypoints = new osg::Group();
+    
+    addWaypoints(waypoints);
+
+    return waypoints;
 }
 
-void WaypointVisualization::updateMainNode( osg::Node* node )
+void WaypointVisualization::updateMainNode ( osg::Node* node )
 {
-    sphere->setRadius(waypoint.tol_position);
-    sphere->setCenter(osg::Vec3(waypoint.position.x(), waypoint.position.y(), waypoint.position.z()));
-    
-    sphereDrawable->setColor(color);
-    sphereDrawable->dirtyDisplayList();
-    sphereDrawable->dirtyBound();
-    
-    geode->removeDrawable(sphereDrawable);
-    geode->addDrawable(sphereDrawable);
-    geode->dirtyBound();
+    node->asGroup()->removeChildren(0, node->asGroup()->getNumChildren());
+    addWaypoints(node->asGroup());
 }
 
+void WaypointVisualization::updateDataIntern(std::vector<base::Waypoint> const& data)
+{
+    p->data = data;
+}
+
+void  WaypointVisualization::updateDataIntern ( base::Waypoint const& data ) {
+    p->data.clear();
+    p->data.push_back(data);
+}
+
+void WaypointVisualization::addWaypoints(osg::Group* group) {
+    std::vector<base::Waypoint>::iterator it = p->data.begin();
+    for(; it != p->data.end(); ++it) {
+
+        // Create sphere.
+        osg::ref_ptr<osg::Sphere> sp = new osg::Sphere(osg::Vec3d(0,0,0), 0.1);
+        osg::ref_ptr<osg::ShapeDrawable> sd = new osg::ShapeDrawable(sp.get());
+        sd->setColor(color);
+        
+        // Create triangle.
+        osg::ref_ptr<osg::Geometry> triangle_geometry = new osg::Geometry();
+        osg::ref_ptr<osg::Vec3Array> triangle_vertices = new osg::Vec3Array();
+        triangle_vertices->push_back(osg::Vec3(0.2,0,0));
+        triangle_vertices->push_back(osg::Vec3(0,0.6,0));
+        triangle_vertices->push_back(osg::Vec3(-0.2,0,0));
+        triangle_geometry->setVertexArray(triangle_vertices);
+        osg::ref_ptr<osg::DrawElementsUInt> triangle_face = 
+                new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+        triangle_face->push_back(0);
+        triangle_face->push_back(1);
+        triangle_face->push_back(2);
+        triangle_geometry->addPrimitiveSet(triangle_face);
+        osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
+        colors->push_back( color );
+        triangle_geometry->setColorArray(colors);
+        triangle_geometry->setColorBinding(osg::Geometry::BIND_OVERALL);
+        
+        // Add drawables to the geode.
+        osg::ref_ptr<osg::Geode> waypoint_geode = new osg::Geode();
+        waypoint_geode->addDrawable(sd.get());
+        waypoint_geode->addDrawable(triangle_geometry);
+        
+        // Create transformer.
+        osg::ref_ptr<osg::PositionAttitudeTransform> waypoint_transform = 
+                new osg::PositionAttitudeTransform();
+        osg::Vec3 position = eigenVectorToOsgVec3(it->position);
+        position[2] += 0.01; // Moves the waypoints a little bit above the z=0 plane.
+        waypoint_transform->setPosition(position);
+        waypoint_transform->setAttitude(osg::Quat(0,0,1,it->heading));
+        waypoint_transform->addChild(waypoint_geode);
+        
+        // Adds the waypoints to the main node.
+        group->addChild(waypoint_transform);
+    }
 }
