f6b25aae276cebf34e6075048da0deeba553b374
Malte Langosz <malte.langosz@dfki.de>
2015-02-11 16:38:49 +0100

    made sisl optional also for the ruby binding

--- bindings/ruby/ext/base_types_ruby/init.cpp
+++ bindings/ruby/ext/base_types_ruby/init.cpp
@@ -1,10 +1,15 @@
 #include "rice/Class.hpp"
 extern void Init_eigen_ext();
+
+#ifdef SISL_FOUND
 extern void Init_spline_ext();
+#endif
 
 extern "C" void Init_base_types_ruby()
 {
     Init_eigen_ext();
+#ifdef SISL_FOUND
     Init_spline_ext();
+#endif
 }
 
