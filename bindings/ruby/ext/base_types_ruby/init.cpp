6b83ba992ab6a490f13ddfacad70c407637d8b6e
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-07-12 14:19:00 -0300

    remove the Eigen bindings
    They have been moved in a separate package/gem

--- bindings/ruby/ext/base_types_ruby/init.cpp
+++ bindings/ruby/ext/base_types_ruby/init.cpp
@@ -1,5 +1,4 @@
 #include "rice/Class.hpp"
-extern void Init_eigen_ext();
 
 #ifdef SISL_FOUND
 extern void Init_spline_ext();
@@ -7,7 +6,6 @@ extern void Init_spline_ext();
 
 extern "C" void Init_base_types_ruby()
 {
-    Init_eigen_ext();
 #ifdef SISL_FOUND
     Init_spline_ext();
 #endif
