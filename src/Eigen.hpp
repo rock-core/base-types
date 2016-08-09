8f6f83e3e1c2a5073bdc08f1cdb3ae5673c52f52
Dennis Hemker <dhem2012@informatik.uni-bremen.de>
2016-08-09 14:41:39 +0200

    moved guaranteeSPD to TwistWithCovariance only

--- src/Eigen.hpp
+++ src/Eigen.hpp
@@ -4,7 +4,6 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry> 
 
-#include <Eigen/Eigenvalues>
 
 namespace base
 {
@@ -53,31 +52,6 @@ namespace base
     {
         return isnotnan(x - x);
     };
-
-    /**
-     * Guarantee Symmetric (semi-) Positive Definite (SPD) matrix.
-     * The input matrix must be symmetric already (only the lower triangular part will be considered)
-     */
-    template <typename _MatrixType>
-    static typename _MatrixType::PlainObject guaranteeSPD (const _MatrixType &A, const typename _MatrixType::RealScalar& minEig = 0.0)
-    {
-        typedef typename _MatrixType::PlainObject Matrix;
-        typedef Eigen::SelfAdjointEigenSolver<Matrix> EigenDecomp;
-        typedef typename EigenDecomp::RealVectorType EigenValues;
-
-        // Eigenvalue decomposition
-        Eigen::SelfAdjointEigenSolver<Matrix> eigOfA (A, Eigen::ComputeEigenvectors);
-
-        // truncate Eigenvalues:
-        EigenValues s = eigOfA.eigenvalues();
-        s = s.cwiseMax(minEig);
-
-        // re-compose matrix:
-        Matrix spdA = eigOfA.eigenvectors() * s.asDiagonal() * eigOfA.eigenvectors().adjoint();
-
-        return spdA;
-    };
-
 
 }
 
