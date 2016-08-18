d5d5d11095e79ccb1b41edc4fe59cb5bcf22e6a0
Dennis Hemker <dhem2012@informatik.uni-bremen.de>
2016-08-18 12:06:10 +0200

    removed deprecated CompressedFrame

--- src/samples/CompressedFrame.cpp
+++
@@ -1,9 +0,0 @@
-#include "CompressedFrame.hpp"
-
-base::samples::frame::frame_compressed_mode_t base::samples::frame::CompressedFrame::toFrameMode(const std::string& str)
-{
-    if(str == "MODE_COMPRESSED_UNDEFINED")
-        return frame_compressed_mode_t::MODE_COMPRESSED_UNDEFINED;
-    else if (str == "MODE_PJPG")
-        return frame_compressed_mode_t::MODE_PJPG;
-}
