04736ed6c625feddbd7511d1491ac1564b981300
Dennis Hemker <dhem2012@informatik.uni-bremen.de>
2016-09-06 16:26:57 +0200

    moved guaranteeSPD to own header

--- src/TwistWithCovariance.cpp
+++ src/TwistWithCovariance.cpp
@@ -1,39 +1,12 @@
 #include "TwistWithCovariance.hpp"
 
 #include <iomanip> // std::setprecision
-
-#include <Eigen/Core>
-#include <Eigen/LU>
-#include <Eigen/Geometry>
-#include <Eigen/Eigenvalues>
-
+#include <Eigen/Core>   
 #include <base/Float.hpp>
 
+#include "Matrix.hpp"
+
 namespace base {
-
-/**
- * Guarantee Symmetric (semi-) Positive Definite (SPD) matrix.
- * The input matrix must be symmetric already (only the lower triangular part will be considered)
- */
-template <typename _MatrixType>
-static typename _MatrixType::PlainObject guaranteeSPD (const _MatrixType &A, const typename _MatrixType::RealScalar& minEig = 0.0)
-{
-    typedef typename _MatrixType::PlainObject Matrix;
-    typedef Eigen::SelfAdjointEigenSolver<Matrix> EigenDecomp;
-    typedef typename EigenDecomp::RealVectorType EigenValues;
-
-    // Eigenvalue decomposition
-    Eigen::SelfAdjointEigenSolver<Matrix> eigOfA (A, Eigen::ComputeEigenvectors);
-
-    // truncate Eigenvalues:
-    EigenValues s = eigOfA.eigenvalues();
-    s = s.cwiseMax(minEig);
-
-    // re-compose matrix:
-    Matrix spdA = eigOfA.eigenvectors() * s.asDiagonal() * eigOfA.eigenvectors().adjoint();
-
-    return spdA;
-};
 
 
 typedef TwistWithCovariance::Covariance Covariance;
