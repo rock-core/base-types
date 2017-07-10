662f7290a5feb2c93d7c046d3e0c033dea91cc06
Alexander Fabisch <alexander.fabisch@dfki.de>
2017-07-10 16:59:54 +0200

    Don't copy data

--- bindings/python/basetypes.pyx
+++ bindings/python/basetypes.pyx
@@ -2,20 +2,24 @@
 cimport _basetypes
 from cython.operator cimport dereference as deref
 from libcpp.string cimport string
+from libcpp cimport bool
 
 
 cdef class PyTime:
     cdef _basetypes.Time* thisptr
+    cdef bool delete_thisptr
 
     def __cinit__(self):
         self.thisptr = NULL
+        self.delete_thisptr = False
 
     def __dealloc__(self):
-        if self.thisptr != NULL:
+        if self.thisptr != NULL and self.delete_thisptr:
             del self.thisptr
 
     def __init__(self):
         self.thisptr = new _basetypes.Time()
+        self.delete_thisptr = True
 
     def _get_microseconds(self):
         return self.thisptr.microseconds
@@ -28,16 +32,19 @@ cdef class PyTime:
 
 cdef class PyVector3d:
     cdef _basetypes.Vector3d* thisptr
+    cdef bool delete_thisptr
 
     def __cinit__(self):
         self.thisptr = NULL
+        self.delete_thisptr = False
 
     def __dealloc__(self):
-        if self.thisptr != NULL:
+        if self.thisptr != NULL and self.delete_thisptr:
             del self.thisptr
 
     def __init__(self, double x=0.0, double y=0.0, double z=0.0):
         self.thisptr = new _basetypes.Vector3d(x, y, z)
+        self.delete_thisptr = True
 
     def __str__(self):
         return "[%.2f, %.2f, %.2f]" % (self.thisptr.get(0),
@@ -56,16 +63,19 @@ cdef class PyVector3d:
 
 cdef class PyQuaterniond:
     cdef _basetypes.Quaterniond* thisptr
+    cdef bool delete_thisptr
 
     def __cinit__(self):
         self.thisptr = NULL
+        self.delete_thisptr = False
 
     def __dealloc__(self):
-        if self.thisptr != NULL:
+        if self.thisptr != NULL and self.delete_thisptr:
             del self.thisptr
 
     def __init__(self, double w=1.0, double x=0.0, double y=0.0, double z=0.0):
         self.thisptr = new _basetypes.Quaterniond(w, x, y, z)
+        self.delete_thisptr = True
 
     def __str__(self):
         return "[im=%.2f, real=(%.2f, %.2f, %.2f)]" % (
@@ -75,20 +85,25 @@ cdef class PyQuaterniond:
 
 cdef class PyTransformWithCovariance:
     cdef _basetypes.TransformWithCovariance* thisptr
+    cdef bool delete_thisptr
 
     def __cinit__(self):
         self.thisptr = NULL
+        self.delete_thisptr = False
 
     def __dealloc__(self):
-        if self.thisptr != NULL:
+        if self.thisptr != NULL and self.delete_thisptr:
             del self.thisptr
 
     def __init__(self):
         self.thisptr = new _basetypes.TransformWithCovariance()
+        self.delete_thisptr = True
 
     def _get_translation(self):
-        translation = PyVector3d()
-        translation.thisptr[0] = self.thisptr.translation
+        cdef PyVector3d translation = PyVector3d()
+        del translation.thisptr
+        translation.delete_thisptr = False
+        translation.thisptr = &self.thisptr.translation
         return translation
 
     def _set_translation(self, PyVector3d translation):
@@ -97,8 +112,10 @@ cdef class PyTransformWithCovariance:
     translation = property(_get_translation, _set_translation)
 
     def _get_orientation(self):
-        orientation = PyQuaterniond()
-        orientation.thisptr[0] = self.thisptr.orientation
+        cdef PyQuaterniond orientation = PyQuaterniond()
+        del orientation.thisptr
+        orientation.delete_thisptr = False
+        orientation.thisptr = &self.thisptr.orientation
         return orientation
 
     def _set_orientation(self, PyQuaterniond orientation):
