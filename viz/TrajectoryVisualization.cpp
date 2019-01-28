587c90fe10ca8d1195d25ca34ea0840cd61f7db1
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2019-01-28 14:18:20 -0200

    viz: fix handling of the LineWidth attribute
    The line width would be applied only if setLineWidth was called
    *after* the creation of the main node. Move it to updateMainNode,
    and fix lack of synchronization between the setters and updateMainNode

--- viz/TrajectoryVisualization.cpp
+++ viz/TrajectoryVisualization.cpp
@@ -1,13 +1,14 @@
 #include "TrajectoryVisualization.hpp"
 #include <osg/Geometry>
 #include <osg/Geode>
-#include <osg/LineWidth>
 
-namespace vizkit3d 
+namespace vizkit3d
 {
 
 TrajectoryVisualization::TrajectoryVisualization()
-    : doClear(false), max_number_of_points(1800), line_width( 1.0 ), color(1., 0., 0., 1.), backwardColor(1., 0., 1., 1.)
+    : doClear(false), max_number_of_points(1800)
+    , line_width( 1.0 )
+    , color(1., 0., 0., 1.), backwardColor(1., 0., 1., 1.)
 {
     VizPluginRubyMethod(TrajectoryVisualization, base::Vector3d, setColor);
 }
@@ -25,15 +26,16 @@ osg::ref_ptr<osg::Node> TrajectoryVisualization::createMainNode()
     geom->setVertexArray(pointsOSG.get());
     drawArrays = new osg::DrawArrays( osg::PrimitiveSet::LINE_STRIP, 0, pointsOSG->size() );
     geom->addPrimitiveSet(drawArrays.get());
+    lineWidth = new osg::LineWidth;
 
     // Add the Geometry (Drawable) to a Geode and
     //   return the Geode.
-    geode = new osg::Geode;
+    osg::Geode* geode = new osg::Geode;
     geode->addDrawable( geom.get() );
 
     osg::StateSet* stategeode = geode->getOrCreateStateSet();
     stategeode->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
-    
+
     return geode;
 }
 
@@ -52,11 +54,13 @@ void TrajectoryVisualization::clear()
 }
 
 void TrajectoryVisualization::updateMainNode( osg::Node* node )
-{   
-
-    osg::StateSet* stategeode = geode->getOrCreateStateSet();
+{
+    osg::StateSet* stategeode = node->getOrCreateStateSet();
     stategeode->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
-    
+
+    boost::mutex::scoped_lock lockit(this->updateMutex);
+    lineWidth->setWidth(line_width);
+
     pointsOSG->clear();
     colorArray->clear();
     for(std::deque<Point>::iterator it = points.begin(); it != points.end(); it++)
@@ -64,7 +68,7 @@ void TrajectoryVisualization::updateMainNode( osg::Node* node )
         pointsOSG->push_back(it->point);
         colorArray->push_back(it->color);
     }
-    
+
     geom->setVertexArray(pointsOSG);
     drawArrays->setCount(pointsOSG->size());
     geom->setColorArray(colorArray);
@@ -75,11 +79,11 @@ void TrajectoryVisualization::addSpline(const base::geometry::Spline3& data,
                                         const osg::Vec4& color)
 {
     //needs a copy as getCurveLength is not const
-    base::geometry::Spline3 spline = data; 
-    
+    base::geometry::Spline3 spline = data;
+
     if(!data.getSISLCurve())
 	return;
-    
+
     //a point every 5 cm
     double stepSize = (spline.getEndParam() - spline.getStartParam()) / (spline.getCurveLength() / 0.05);
     for(double param = spline.getStartParam(); param <= spline.getEndParam(); param += stepSize )
@@ -132,6 +136,7 @@ void TrajectoryVisualization::updateDataIntern( const base::Vector3d& data )
 
 void TrajectoryVisualization::setColor(QColor color)
 {
+    boost::mutex::scoped_lock lockit(this->updateMutex);
     this->color = osg::Vec4(color.redF(), color.greenF(), color.blueF(), color.alphaF());
     emit propertyChanged("Color");
     setDirty();
@@ -146,6 +151,7 @@ QColor TrajectoryVisualization::getColor() const
 
 void TrajectoryVisualization::setBackwardColor(QColor c)
 {
+    boost::mutex::scoped_lock lockit(this->updateMutex);
     backwardColor = osg::Vec4(c.redF(), c.greenF(), c.blueF(), c.alphaF() );
     emit propertyChanged("BackwardColor");
     setDirty();
@@ -165,15 +171,9 @@ double TrajectoryVisualization::getLineWidth()
 
 void TrajectoryVisualization::setLineWidth(double line_width)
 {
+    boost::mutex::scoped_lock lockit(this->updateMutex);
     this->line_width = line_width;
-    if(geode)
-    {
-        osg::StateSet* stateset = geode->getOrCreateStateSet();
-        osg::LineWidth* linewidth = new osg::LineWidth();
-        linewidth->setWidth(line_width);
-        stateset->setAttributeAndModes(linewidth,osg::StateAttribute::ON);
-        stateset->setMode(GL_LIGHTING,osg::StateAttribute::OFF);
-    }
     emit propertyChanged("LineWidth");
+    setDirty();
 }
 }
