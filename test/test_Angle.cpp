6dfcee0fb797b8fad76af630733b21861ca29cb0
Sylvain <sylvain.joyeux@tidewise.io>
2019-12-09 13:12:08 -0300

    test: rewrite the AngleSegment test
    The last test (loop in a loop) was very obscure specification-wise,
    and was actually broken - only that its broken-ness was not showing
    up because the test writer used assert() instead of test assertions,
    which were elided by the compiler since we compile with optimizations.
    
    Rewrite the test trying to follow all the possible corner-cases.

--- test/test_Angle.cpp
+++ test/test_Angle.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 using namespace base;
+namespace utf = boost::unit_test;
 
 BOOST_AUTO_TEST_SUITE(AngleTests)
 
@@ -107,187 +108,174 @@ BOOST_AUTO_TEST_CASE( vectorToVector_handles_cos_rounded_below_1 )
     BOOST_CHECK_CLOSE(Angle::vectorToVector(v, -v, Vector3d::UnitZ()).getRad(), M_PI, 1e-3);
 }
 
-BOOST_AUTO_TEST_CASE( angle_segment_test )
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(AngleSegmentTests)
+
+void angleIsInsideSegmentTest(Angle a) {
+    Angle delta = base::Angle::fromRad(1e-3);
+    Angle just_before = a - 2 * delta;
+    Angle just_after = a + 2 * delta;
+
+    AngleSegment s;
+
+    // "Easy" non-pathological tests
+    s = AngleSegment(just_before, (2 * delta).getRad());
+    BOOST_TEST(s.isInside(a));
+    s = AngleSegment(just_before, (0.9 * delta).getRad());
+    BOOST_TEST(!s.isInside(a));
+
+    // End of segment is exactly pi
+    s = AngleSegment(just_before, M_PI - just_before.getRad());
+    BOOST_TEST(s.isInside(a));
+    // End of segment is -180 degrees
+    s = AngleSegment(just_before, M_PI - just_before.getRad() + 1e-3);
+    BOOST_TEST(s.isInside(a));
+    // End of segment is "just before" the angle, segment being almost a full turn
+    s = AngleSegment(just_after, 2 * M_PI - 3 * delta.getRad());
+    BOOST_TEST(!s.isInside(a));
+}
+
+void segmentIsInsideTest(Angle min, double length) {
+    AngleSegment s(min, length);
+    Angle delta = base::Angle::fromRad(1e-3);
+    double delta_rad = delta.getRad();
+    double min_rad = min.getRad();
+
+    // The delta value to use to go "in". Handles length < delta
+    double delta_in_rad = std::min(length / 2, delta_rad);
+
+    // "Easy" non-pathological tests
+    BOOST_TEST(!s.isInside(Angle::fromRad(min_rad - delta_rad)));
+    BOOST_TEST(!s.isInside(Angle::fromRad(min_rad + length + delta_rad)));
+    BOOST_TEST(s.isInside(Angle::fromRad(min_rad + length / 2)));
+    BOOST_TEST(s.isInside(Angle::fromRad(min_rad + delta_in_rad)));
+    BOOST_TEST(s.isInside(Angle::fromRad(min_rad + length - delta_in_rad)));
+
+    // Angle is at the diametrically opposed position
+    BOOST_TEST(!s.isInside(Angle::fromRad(min_rad + length / 2 + M_PI)));
+}
+
+void REQUIRE_SEGMENTS_EQUAL(AngleSegment s1, AngleSegment s2) {
+    BOOST_TEST(s1.startRad == s2.startRad);
+    BOOST_TEST(s1.width == s2.width);
+    BOOST_TEST(s1.endRad == s2.endRad);
+}
+
+void REQUIRE_INTERSECTIONS_EQUAL(vector<AngleSegment> const& v1,
+                                 vector<AngleSegment> const& v2) {
+    BOOST_TEST(v1.size() == v2.size());
+    for (size_t i = 0; i < v1.size(); ++i) {
+        REQUIRE_SEGMENTS_EQUAL(v1[i], v2[i]);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(angle_segment_test_isInside)
 {
-    Angle a = Angle::fromDeg( 90 );
-    {
-        AngleSegment s(Angle::fromDeg(89), Angle::fromDeg(2).getRad());
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(89), (180-89) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(89), (190-89) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(89), (360-89) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(91), (360-2) / 180.0 * M_PI);
-        BOOST_CHECK(! s.isInside(a) );
-    }
-    a = Angle::fromDeg( 190 );
-    {
-        AngleSegment s(Angle::fromDeg(170), (30) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(185), (15) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(170), (350) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(170), (350-170) / 180.0 * M_PI);
-        BOOST_CHECK( s.isInside(a) );
-        s = AngleSegment(Angle::fromDeg(200), (380-200) / 180.0 * M_PI);
-        BOOST_CHECK(! s.isInside(a) );
-    }
-    {
-        AngleSegment s(Angle::fromDeg(20), Angle::fromDeg(45).getRad());
-        BOOST_CHECK( s.isInside(Angle::fromDeg(20)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(30)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(65)));
-        BOOST_CHECK(! s.isInside(Angle::fromDeg(19)));
-        BOOST_CHECK(! s.isInside(Angle::fromDeg(66)));
-    }
+    angleIsInsideSegmentTest(Angle::fromDeg(90));
+    angleIsInsideSegmentTest(Angle::fromDeg(190));
+    segmentIsInsideTest(Angle::fromDeg(350), Angle::fromDeg(45).getRad());
+    segmentIsInsideTest(Angle::fromDeg(160), Angle::fromDeg(45).getRad());
+}
 
-    {
-        AngleSegment s(Angle::fromDeg(350), Angle::fromDeg(45).getRad());
-        BOOST_CHECK( s.isInside(Angle::fromDeg(20)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(35)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(350)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(355)));
-        BOOST_CHECK(! s.isInside(Angle::fromDeg(36)));
-        BOOST_CHECK(! s.isInside(Angle::fromDeg(349)));
-    }
+BOOST_AUTO_TEST_CASE(
+    it_properly_intersects_with_itself_a_segment_that_does_not_cross_the_180_line
+) {
+    AngleSegment s(Angle::fromDeg(-20), Angle::deg2Rad(45));
+    vector<AngleSegment> expected { s };
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s.getIntersections(s));
+}
 
-    {
-        AngleSegment s(Angle::fromDeg(160), Angle::fromDeg(45).getRad());
-        BOOST_CHECK( s.isInside(Angle::fromDeg(160)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(180)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(181)));
-        BOOST_CHECK( s.isInside(Angle::fromDeg(205)));
-        BOOST_CHECK(! s.isInside(Angle::fromDeg(159)));
-        BOOST_CHECK(! s.isInside(Angle::fromDeg(206)));
-    }
+BOOST_AUTO_TEST_CASE(it_finds_intersection_with_segments_that_do_not_cross_the_180_line,
+                     * utf::tolerance(0.0001))
+{
+    vector<AngleSegment> expected { AngleSegment(Angle::fromDeg(20), Angle::deg2Rad(5)) };
+    AngleSegment s1(Angle::fromDeg(-20), Angle::deg2Rad(45));
+    AngleSegment s2(Angle::fromDeg(20), Angle::deg2Rad(45));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s1.getIntersections(s2));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s2.getIntersections(s1));
+}
 
-    {
-        AngleSegment result(Angle::fromDeg(0), 0);
-        AngleSegment s1(Angle::fromDeg(160), Angle::fromDeg(45).getRad());
-        AngleSegment s2(Angle::fromDeg(100), Angle::fromDeg(45).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        BOOST_CHECK(ret.size() == 0);
-    }
-    {
-        AngleSegment s1(Angle::fromDeg(160), Angle::fromDeg(45).getRad());
-        AngleSegment s2(Angle::fromDeg(180), Angle::fromDeg(45).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 1);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(180).getRad()) < 0.0001);
-        BOOST_CHECK(fabs(result.getWidth() - Angle::fromDeg(25).getRad()) < 0.0001);
-    }
-    {
-        AngleSegment s1(Angle::fromDeg(180), Angle::fromDeg(45).getRad());
-        AngleSegment s2(Angle::fromDeg(220), Angle::fromDeg(45).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 1);
-        BOOST_CHECK(fabs(result.getWidth() - Angle::fromDeg(5).getRad()) < 0.0001);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(220).getRad()) < 0.0001);
-    }
-    {
-        AngleSegment s1(Angle::fromDeg(160), Angle::fromDeg(45).getRad());
-        AngleSegment s2(Angle::fromDeg(165), Angle::fromDeg(20).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 1);
-        BOOST_CHECK(fabs(result.getWidth() - Angle::fromDeg(20).getRad()) < 0.0001);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(165).getRad()) < 0.0001);
-    }
-    {
-        AngleSegment s1(Angle::fromDeg(160), 2*M_PI);
-        AngleSegment s2(Angle::fromDeg(180), Angle::fromDeg(45).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 1);
-        BOOST_CHECK(fabs(result.getWidth() - Angle::fromDeg(45).getRad()) < 0.0001);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(180).getRad()) < 0.0001);
-    }
+BOOST_AUTO_TEST_CASE(
+    it_finds_intersection_with_overlapping_segments_that_do_not_cross_the_180_line,
+    * utf::tolerance(0.0001)
+) {
+    vector<AngleSegment> expected {
+        AngleSegment(Angle::fromDeg(20), Angle::deg2Rad(45))
+    };
+    AngleSegment s1(Angle::fromDeg(0), Angle::deg2Rad(70));
+    AngleSegment s2(Angle::fromDeg(20), Angle::deg2Rad(45));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s1.getIntersections(s2));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s2.getIntersections(s1));
+}
 
-    {
-        AngleSegment s1(Angle::fromDeg(-20), 2*M_PI);
-        AngleSegment s2(Angle::fromDeg(50), Angle::fromDeg(45).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 1);
-        BOOST_CHECK(fabs(result.getWidth() - Angle::fromDeg(45).getRad()) < 0.0001);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(50).getRad()) < 0.0001);
-    }
-    {
-        AngleSegment s1(Angle::fromDeg(-20), 2*M_PI);
-        AngleSegment s2(Angle::fromDeg(0), 2*M_PI);
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 1);
-        BOOST_CHECK(fabs(result.getWidth() - 2*M_PI) < 0.0001);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(0).getRad()) < 0.0001);
-    }
-    {
-        AngleSegment s1(Angle::fromDeg(-20), 2*M_PI - Angle::fromDeg(1).getRad());
-        AngleSegment s2(Angle::fromDeg(0), 2*M_PI - Angle::fromDeg(1).getRad());
-        vector<AngleSegment> ret = s1.getIntersections(s2);
-        AngleSegment result = ret[0];
-        BOOST_CHECK(ret.size() == 2);
-        BOOST_CHECK(fabs(result.getWidth() - (2*M_PI - Angle::fromDeg(21).getRad())) < 0.0001);
-        BOOST_CHECK(fabs(result.startRad - Angle::fromDeg(0).getRad()) < 0.0001);
-    }
+BOOST_AUTO_TEST_CASE(it_handles_non_intersecting_segments_that_do_not_cross_the_180_line,
+                     * utf::tolerance(0.0001))
+{
+    vector<AngleSegment> expected { AngleSegment(Angle::fromDeg(20), Angle::deg2Rad(5)) };
+    AngleSegment s1(Angle::fromDeg(-30), Angle::fromDeg(45).getRad());
+    AngleSegment s2(Angle::fromDeg(20), Angle::fromDeg(45).getRad());
+    BOOST_TEST(s1.getIntersections(s2).empty());
+    BOOST_TEST(s2.getIntersections(s1).empty());
+}
 
-    {
-        for(int x = 0; x < 360; x++)
-        {
-            for(int y = 0; y < 360; y++)
-            {
-                AngleSegment s1(Angle::fromDeg(x), 2*M_PI - Angle::fromDeg(1).getRad());
-                AngleSegment s2(Angle::fromDeg(y), 2*M_PI - Angle::fromDeg(1).getRad());
-                vector<AngleSegment> ret = s1.getIntersections(s2);
-                double sum = 0;
-                for(vector<AngleSegment>::const_iterator it = ret.begin(); it != ret.end(); it++)
-                {
-                    sum += it->width;
-                }
-                if(abs(x-y) < 2 || (x == 0 && y == 359 )|| (y == 0 && x == 359))
-                {
-                    if(ret.size() != 1)
-                    {
-                        ostringstream out;
-                        for(vector<AngleSegment>::const_iterator it = ret.begin(); it != ret.end(); it++)
-                        {
-                            out << "A1 " << x << " A2 " << y << " Result 8" << *it << endl;
-                        }
-                        BOOST_TEST_MESSAGE(out.str());
-                        BOOST_FAIL("one intersection was expected, got " + to_string(ret.size()));
-                    }
 
-                    double diff = fabs((2*M_PI - Angle::fromDeg(1).getRad()) - sum);
-                    if (diff >= 0.000001) {
-                        BOOST_REQUIRE(diff < 0.000001);
-                    }
-                }
-                else
-                {
-                    if(ret.size() != 2)
-                    {
-                        ostringstream out;
-                        for(vector<AngleSegment>::const_iterator it = ret.begin(); it != ret.end(); it++)
-                        {
-                            out << "A1 " << x << " A2 " << y << " Result 8" << *it << endl;
-                        }
-                        BOOST_TEST_MESSAGE(out.str());
-                        BOOST_FAIL("two intersections were expected, got " + to_string(ret.size()));
-                    }
 
-                    double diff = fabs((2*M_PI - Angle::fromDeg(2).getRad()) - sum);
-                    if (diff >= 0.000001) {
-                        BOOST_REQUIRE(diff < 0.000001);
-                    }
-                }
-            }
-        }
-    }
+BOOST_AUTO_TEST_CASE(
+    it_properly_intersects_with_itself_a_segment_that_crosses_the_180_line
+) {
+    AngleSegment s(Angle::fromDeg(120), Angle::deg2Rad(90));
+    vector<AngleSegment> expected { s };
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s.getIntersections(s));
+}
+
+BOOST_AUTO_TEST_CASE(it_finds_intersection_with_one_segment_that_crosses_the_180_line,
+                     * utf::tolerance(0.0001))
+{
+    vector<AngleSegment> expected {
+        AngleSegment(Angle::fromDeg(120), Angle::deg2Rad(10))
+    };
+    AngleSegment s1(Angle::fromDeg(120), Angle::deg2Rad(70));
+    AngleSegment s2(Angle::fromDeg(20), Angle::deg2Rad(110));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s1.getIntersections(s2));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s2.getIntersections(s1));
+}
+
+BOOST_AUTO_TEST_CASE(
+    it_finds_intersection_with_overlapping_segments_that_cross_the_180_line,
+    * utf::tolerance(0.0001)
+) {
+    vector<AngleSegment> expected {
+        AngleSegment(Angle::fromDeg(120), Angle::deg2Rad(70))
+    };
+    AngleSegment s1(Angle::fromDeg(120), Angle::deg2Rad(70));
+    AngleSegment s2(Angle::fromDeg(110), Angle::deg2Rad(90));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s1.getIntersections(s2));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s2.getIntersections(s1));
+}
+
+BOOST_AUTO_TEST_CASE(
+    it_handles_non_intersecting_segments_with_one_segment_that_crosses_the_180_line,
+    * utf::tolerance(0.0001)
+) {
+    AngleSegment s1(Angle::fromDeg(140), Angle::deg2Rad(70));
+    AngleSegment s2(Angle::fromDeg(20), Angle::deg2Rad(110));
+    BOOST_TEST(s1.getIntersections(s2).empty());
+    BOOST_TEST(s2.getIntersections(s1).empty());
+}
+
+BOOST_AUTO_TEST_CASE(
+    it_handles_ranges_that_intersect_twice,
+    * utf::tolerance(0.0001)
+) {
+    vector<AngleSegment> expected {
+        AngleSegment(Angle::fromDeg(80), Angle::deg2Rad(10)),
+        AngleSegment(Angle::fromDeg(-110), Angle::deg2Rad(10))
+    };
+    AngleSegment s1(Angle::fromDeg(-110), Angle::deg2Rad(200));
+    AngleSegment s2(Angle::fromDeg(80), Angle::deg2Rad(180));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s1.getIntersections(s2));
+    REQUIRE_INTERSECTIONS_EQUAL(expected, s2.getIntersections(s1));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
