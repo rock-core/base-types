c8c101024f50c06fbdb274edc236901dbf8e2ed7
Henrique Ferreira <henrique.ferreira@tidewise.io>
2020-02-11 17:16:03 -0300

    Create a Temperature sample.
    Basically it is adding a timestamp to it.

---
+++ src/samples/Temperature.hpp
@@ -0,0 +1,30 @@
+#ifndef __BASE_SAMPLES_TEMPERATURE_HH__
+#define __BASE_SAMPLES_TEMPERATURE_HH__
+
+#include <base/Temperature.hpp>
+#include <base/Time.hpp>
+
+namespace base
+{
+    namespace samples
+    {
+        struct Temperature : public base::Temperature
+        {
+            /** The sample timestamp */
+            base::Time time;
+
+            Temperature() : base::Temperature() { }
+
+            Temperature(base::Time const& time, base::Temperature temp)
+                : base::Temperature(temp.getKelvin())
+                , time(time) { }
+
+            static Temperature fromKelvin(base::Time const& time, double kelvin);
+
+            static Temperature fromCelsius(base::Time const& time, double celsius);
+
+        };
+    }
+}
+
+#endif
